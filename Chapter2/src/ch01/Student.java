67978a645bcda138dbcba52383402b69b6feccfa
Kimjunho <widn45@naver.com>
2021-05-15 17:05:32 +0900

    day1

---
+++ Chapter2/src/ch01/Student.java
@@ -0,0 +1,14 @@
+package ch01;
+
+//class안에 클래스 만들수 있다.
+class Abc{//단 public 키워드를 가진 클래스는 하나여야만 한다.
+	
+}
+public class Student {//클래스 이름 시작은 대문자, 중간 단어가 바뀔때마다 대문자
+	int studentNumber;//맴버변수 ,접근제어자 사용가능(public,private..) : 어디서 클래스를 접근 가능한지 명시
+	String studentName;
+	int majorCode;
+	String majorName;
+	int grade;
+	
+}
